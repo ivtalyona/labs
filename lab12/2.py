380d58a3a2e86df705f30f00d3fd96c7b64cfbf8
ivtalyona <91089909+ivtalyona@users.noreply.github.com>
2021-11-21 20:15:03 +0300

    Add files via upload

---
+++ lab12/2.py
@@ -0,0 +1,8 @@
+print ('Введите команду для робота 0, 1 или -1')
+N=int(input())
+if N==0:
+    print('C')
+elif N==1:
+    print ('З')
+else:
+    print ('В')
---
+++ lab12/3.py
@@ -0,0 +1,8 @@
+print ('Введите количество учебник заданий от 10 до 40')
+k=int(input())
+if k:
+    print('C')
+elif N==1:
+    print ('З')
+else:
+    print ('В')
